7a0894229d176d9919a72faf5f7e62870f490532
Douglas Braga <douglasmartinsbraga@gmail.com>
2021-04-08 10:09:28 -0300

    add data log

--- get_listings/_log.py
+++ get_listings/_log.py
@@ -1,15 +1,18 @@
 import logging
+from datetime import datetime
 from os.path import join, dirname, abspath
 
 basedir = abspath(join(dirname(__file__), ".."))
 
 
-def setup_logger(file_name: str = "get_listings.log"):
+def setup_logger():
     """
     Classe utilizada para criar o Log
     """
+    date = str(datetime.now().strftime("%Y-%m-%d"))
+
     logging.basicConfig(
-        filename=join(basedir, "logs", file_name),
+        filename=join(basedir, "logs", f"get_listings_{date}.log"),
         format="%(asctime)s, %(module)7s, line: %(lineno)3d, %(levelname)4s: %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
         filemode="a",
