62b06c66b08a7626fa7abfb6502bec60f6ab4c0e
Douglas Braga <douglasmartinsbraga@gmail.com>
2021-08-29 20:23:19 -0300

    melhoria logger

--- listings/extensions/logger.py
+++ listings/extensions/logger.py
@@ -2,9 +2,19 @@ import logging
 
 
 def init_app(app):
-    gunicorn_logger = logging.getLogger("gunicorn.error")
 
-    app.logger.handlers = gunicorn_logger.handlers
-    app.logger.setLevel(gunicorn_logger.level)
+    if app.config["ENV"] != "development":
+        gunicorn_logger = logging.getLogger("gunicorn.error")
+
+        app.logger.handlers = gunicorn_logger.handlers
+        app.logger.setLevel(logging.INFO)
+
+    else:
+        logging.basicConfig(
+            filename="logger.log",
+            format="%(asctime)s %(levelname)8s (%(name)s:%(lineno)s): %(message)s",
+            filemode="w",
+            level=logging.DEBUG,
+        )
 
     return app
