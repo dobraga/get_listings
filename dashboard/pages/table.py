3c936368e37ca9fbcfd7d02d66ef5bddbe52740f
Douglas Braga <douglasmartinsbraga@gmail.com>
2021-09-18 21:25:52 -0300

    renomeia nome das colunas e ordena

--- dashboard/pages/table.py
+++ dashboard/pages/table.py
@@ -7,12 +7,13 @@ from dash.dependencies import Input, Output
 layout = DataTable(
     id="table",
     columns=[
-        {"name": "url", "id": "url"},
-        {"name": "usable_area", "id": "usable_area"},
-        {"name": "type_unit", "id": "type_unit"},
-        {"name": "total_fee", "id": "total_fee"},
-        {"name": "estacao", "id": "estacao"},
-        {"name": "distance", "id": "distance"},
+        {"name": "URL", "id": "url"},
+        {"name": "Área Útil", "id": "usable_area"},
+        {"name": "Tipo", "id": "type_unit"},
+        {"name": "Estação", "id": "estacao"},
+        {"name": "Distância", "id": "distance"},
+        {"name": "Preço Total", "id": "total_fee"},
+        {"name": "Preço Predito", "id": "total_fee_predict"},
     ],
     style_header={"fontWeight": "bold"},
     page_current=0,
@@ -26,9 +27,10 @@ layout = DataTable(
         {"if": {"column_id": "url"}, "maxWidth": 50, "textAlign": "left"},
         {"if": {"column_id": "usable_area"}, "maxWidth": 20},
         {"if": {"column_id": "type_unit"}, "maxWidth": 20, "textAlign": "left"},
-        {"if": {"column_id": "total_fee"}, "maxWidth": 20},
         {"if": {"column_id": "estacao"}, "maxWidth": 50, "textAlign": "left"},
         {"if": {"column_id": "distance"}, "maxWidth": 20},
+        {"if": {"column_id": "total_fee"}, "maxWidth": 20},
+        {"if": {"column_id": "total_fee_predict"}, "maxWidth": 20},
     ],
 )
 
@@ -47,13 +49,19 @@ def init_app(app: Dash) -> Dash:
         if dff.shape[0] == 0:
             return []
 
-        dff["distance"] = dff["distance"].round(0)
+        dff["error"] = dff["total_fee"] - dff["total_fee_predict"]
+
+        dff[["distance", "total_fee_predict"]] = dff[
+            ["distance", "total_fee_predict"]
+        ].round(0)
 
         if len(sort_by):
             dff = dff.sort_values(
                 [col["column_id"] for col in sort_by],
                 ascending=[col["direction"] == "asc" for col in sort_by],
             )
+        else:
+            dff = dff.sort_values("error")
 
         return dff.iloc[
             page_current * page_size : (page_current + 1) * page_size
