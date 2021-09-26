d03f9e0abf483dd6fda72949fac9175e1f4dceea
Douglas Braga <douglasmartinsbraga@gmail.com>
2021-09-26 18:16:35 -0300

    melhoria logger

--- dashboard/extensions/logger.py
+++ dashboard/extensions/logger.py
@@ -6,18 +6,21 @@ def init_app(dash: Dash) -> Dash:
 
     app = dash.server
 
+    log_config = {
+        "level": "DEBUG" if app.config["DEBUG"] else "INFO",
+        "format": "%(asctime)s %(levelname)8s (%(name)s:%(lineno)s): %(message)s",
+        "datefmt": "%Y-%m-%d %H:%M:%S",
+    }
+
     if app.config["ENV"] == "production":
         gunicorn_logger = logging.getLogger("gunicorn.error")
         app.logger.addHandler(gunicorn_logger.handlers)
+    else:
+        log_config["filename"] = "logger.log"
+        log_config["filemode"] = "w"
 
-    level = logging.DEBUG if app.config["DEBUG"] else logging.INFO
+    logging.basicConfig(**log_config)
 
-    logging.basicConfig(
-        format="%(asctime)s %(levelname)8s (%(name)s:%(lineno)s): %(message)s",
-        level=level,
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-
-    app.logger.debug(f"Using config: {app.config}")
+    app.logger.info(f"Using config: {app.config}")
 
     return dash
