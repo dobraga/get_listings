15f939cfe222dc2cefca67a5192bae03b0864672
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2022-02-11 02:13:26 -0300

    use dynaconf

--- backend/settings.py
+++ backend/settings.py
@@ -1,32 +1,7 @@
-from dotenv import load_dotenv, dotenv_values
-from os.path import join, abspath
-from toml import load
-import os
-import re
+from dynaconf import Dynaconf
 
-load_dotenv()
-ENV = dotenv_values()
-
-settings_file = abspath(join(".", "settings.toml"))
-settings_file = abspath(join(__file__, "..", "..", "settings.toml"))
-
-
-def read_settings():
-
-    all_settings = load(settings_file)
-
-    default_settings = all_settings["default"]
-    env_settings = all_settings[os.environ["FLASK_ENV"]]
-
-    settings = {**ENV, **default_settings, **env_settings}
-
-    def format_env(value):
-        if isinstance(value, str) and "@format " in value:
-            value = value.replace("@format ", "").format(**ENV)
-
-        return value
-
-    return {k: format_env(v) for k, v in settings.items()}
-
-
-settings = read_settings()
+settings = Dynaconf(
+    settings_files=["settings.toml"],
+    environments=True,
+    env_switcher="FLASK_ENV",
+)
