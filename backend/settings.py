715e232288218735fe74699f4d50ecfc3c073447
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2022-02-14 09:24:46 -0300

    melhoria nas conigs

--- backend/settings.py
+++ backend/settings.py
@@ -1,7 +1,12 @@
+from pprint import pprint
 from dynaconf import Dynaconf
 
 settings = Dynaconf(
     settings_files=["settings.toml"],
-    environments=True,
     env_switcher="FLASK_ENV",
+    environments=True,
+    load_dotenv=True,
 )
+
+
+pprint(dict(settings))
