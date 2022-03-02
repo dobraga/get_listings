d44cf5169c527d19b47d753ca2b3de13031999b5
Douglas Braga <30268535+dobraga@users.noreply.github.com>
2022-03-02 13:47:28 -0300

    add components and carousel on map

--- dashboard/pages/map.py
+++ dashboard/pages/map.py
@@ -1,4 +1,3 @@
-import locale
 import pandas as pd
 from dash import html, Dash
 from folium import Map, Marker
@@ -6,24 +5,34 @@ from folium.plugins import MarkerCluster
 from dash.exceptions import PreventUpdate
 from dash.dependencies import Input, Output
 
-layout = html.Iframe(
-    id="map", srcDoc=Map(height="89%")._repr_html_(), width="100%", height="800px"
+from dashboard.components.box import box
+
+layout = html.Div(
+    [
+        html.Div(id="qtd_sem_latlon"),
+        html.Iframe(
+            id="map",
+            srcDoc=Map(height="89%")._repr_html_(),
+            width="100%",
+            height="800px",
+        ),
+    ]
 )
-
-locale.setlocale(locale.LC_ALL, "pt_BR.utf8")
-
-
-def to_currency(value):
-    return locale.currency(value, grouping=True, symbol=True)
 
 
 def init_app(app: Dash) -> Dash:
-    @app.callback(Output("map", "srcDoc"), Input("filtered_data", "data"))
+    @app.callback(
+        Output(component_id="qtd_sem_latlon", component_property="children"),
+        Output("map", "srcDoc"),
+        Input("filtered_data", "data"),
+    )
     def create_map(data):
         if not data:
             raise PreventUpdate
 
-        df = pd.DataFrame(data).dropna(subset=["address_lat", "address_lon"])
+        df = pd.DataFrame(data)
+        qtd_sem_latlon = df[["address_lat", "address_lon"]].isna().max(axis=1).sum()
+        df = df.dropna(subset=["address_lat", "address_lon"])
 
         map = Map(
             location=df[["address_lat", "address_lon"]].mean().values,
@@ -34,78 +43,12 @@ def init_app(app: Dash) -> Dash:
 
         marker_cluster = MarkerCluster().add_to(map)
 
-        base_style = "white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"
-
-        def format_template(**kwargs) -> str:
-            if kwargs["business_type"] == "RENTAL":
-
-                if kwargs["condo_fee"] > 0:
-                    preco = """
-                    <p>
-                        <span> Valor {price}/Mês |</span>
-                        <span> Valor Condomínio {condo_fee}/Mês |</span>
-                        <span> total {total_fee}/Mês </span>
-                    </p>
-                    """.format(
-                        price=to_currency(kwargs["price"]),
-                        condo_fee=to_currency(kwargs["condo_fee"]),
-                        total_fee=to_currency(kwargs["total_fee"]),
-                    )
-                else:
-                    preco = """
-                    <p><span> Valor {}/Mês</span></p>
-                    """.format(
-                        to_currency(kwargs["price"])
-                    )
-            else:
-                preco = """
-                <p>
-                    <span> Valor {price} |</span>
-                    <span> Valor Condomínio {condo_fee}/Mês |</span>
-                    <span> total {price} </span>
-                </p>
-                """.format(
-                    price=to_currency(kwargs["price"]),
-                    condo_fee=to_currency(kwargs["condo_fee"]),
-                )
-
-            image = kwargs["images"]
-            if image is not None and isinstance(image, list) and len(image) > 0:
-                image = image[0]
-            else:
-                image = "https://img2.gratispng.com/20180407/yhw/kisspng-empty-set-null-set-symbol-mathematics-forbidden-5ac859ad09c119.24223671152307959704.jpg"
-
-            return """
-            <div onclick="window.open('{url}');" style="cursor: pointer; width: 40vw; height: 20vh;">
-                <div style="float: left; width: 40%">
-                    <img src="{image}" style="width: 100%; height: 100%; ">
-                </div>
-
-                <div style="float: right; width: 58%; height: 100%">
-                    <div style="width: 100%;">
-                        <div title="{address}" style = "{style}"> {address} </div>
-                        <p title="{title}" style = "{style}"> {title} </p>
-                        {preco}
-                        <p style = "{style}" title="{amenities}"> {amenities} </p>
-                    </div>
-                </div>
-            </div>
-            """.format(
-                url=kwargs["url"],
-                address=kwargs["address"],
-                title=kwargs["title"],
-                amenities=kwargs["amenities"],
-                style=base_style,
-                image=image,
-                preco=preco,
-            )
-
         for _, row in df.iterrows():
             Marker(
                 location=[row["address_lat"], row["address_lon"]],
-                popup=format_template(**row),
+                popup=box(**row),
             ).add_to(marker_cluster)
 
-        return map._repr_html_()
+        return f"{qtd_sem_latlon} imóveis sem latitude ou longitude", map._repr_html_()
 
     return app
